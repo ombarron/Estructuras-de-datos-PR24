2a0935b9b5081270ebd4216d7183b8c2adc2004e
Olivia Maricela Barrón Cano <olivia.barron@udem.edu>
2024-01-17 10:55:38 -0600

    primer commit

---
+++ EjVectores.cpp
@@ -0,0 +1,125 @@
+# include <iostream>  // Librería para utilizar cin y cout
+# include <stdlib.h>  // Libreria para poder usar comandos del sistema y funciones de conversion de datos
+# include <vector>
+
+using namespace std; // Para no tener que usar el prefijo std 
+
+// Este programa es un ejemplo de uso de std:vector
+// Permite capturar calificaciones, sacar su promedio, y encontrar la calificación más alta
+// No declara clases nuevas
+
+vector <int> calificaciones;
+
+void pideCalificaciones()
+{  int calificacion, indice = 0;
+   char respuesta; 
+
+   calificaciones.clear();
+
+   do
+   { cout << "Ingrese la calificacion del alumno "<<indice+1<<endl;
+     cin >> calificacion;
+     calificaciones.push_back(calificacion);
+     indice++;
+     cout << "¿Quiere capturar otra calificacion (s/n)?"<<endl;
+     cin>> respuesta;
+   } while (respuesta == 's');
+
+}
+
+ 
+double calculaPromedio()
+{   
+   double promedio = 0.0;
+
+   for (auto& calificacion: calificaciones)
+     promedio += calificacion;
+ 
+   promedio = promedio / calificaciones.size();
+   return promedio;
+}
+
+int encontrarMejor(int &mejor)
+{
+    int contador = -1;
+    int indice = 0;
+    mejor = calificaciones[0];
+
+    for (auto& calificacion: calificaciones)
+    {    
+        contador++;
+        if (calificacion> mejor)
+        { mejor = calificacion;
+          indice = contador;
+         }
+    }
+    return indice;
+}
+    
+    
+// Esta rutina despliega el menu principal
+void despliegaMenu()
+{
+    // Primero limpiamos la pantalla
+    system("cls");
+
+    // Desplegamos las opciones
+    cout << "      01 Pedir calificaciones      " << endl;
+    cout << "      02 Calcular promedio         " << endl;
+    cout << "      03 Encontrar mejor           " << endl;
+    cout << "      04 Salir del programa        " << endl;
+    cout << "                            Opcion ";
+
+}
+
+
+int main()
+{
+    // Declaramos variables de entrada
+    string opcion_str;
+    int opcion, mejor,indice;
+
+    // Todo el código se va a repetir hasta que el usuario indique que quiere terminar
+    do
+    {
+  
+        // Llamamos a la función que dibuja el menu
+        despliegaMenu(); 
+        //Pedimos la opción al usuario, la capturamos como string para no tener problemas si el usurio se equivoca
+        cin >> opcion_str;
+
+        // Cambiamos de string a entero
+        opcion = atoi(opcion_str.c_str());
+
+        // Y utilizamos un switch para ejecutar las diferentes opciones
+        switch (opcion)
+        {
+        case 1:
+            cout << "Pedir calificaciones"<<endl;
+            pideCalificaciones();
+            system("pause");
+            break;
+        case 2:
+            cout << "Calcular promedio"<<endl;
+            cout << "El promedio de las calificaciones es "<<calculaPromedio()<<endl;
+            system("pause");
+            break;
+        case 3:
+            cout << "Encontrar mejor"<<endl;
+            indice = encontrarMejor(mejor);
+            cout << "El alumno # "<<indice+1<<" obtuvo la mejor calificacion que es "<<mejor<<endl;
+            system("pause");
+            break;
+        case 4:
+            cout << "Vamos a salir"<<endl;
+            system("pause");
+            break;
+        default:
+            cout << "Por favor da una opcion valida (1, 2, 3 o 4)"<<endl;
+            system("pause");
+            break;
+        }
+    } while (opcion != 4);
+    
+    return 0;
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Nuevo repositorio para la clase de estructuras de datos
+17 de enero de 2024 Se subio ejemplo sencillo de vectores
+
